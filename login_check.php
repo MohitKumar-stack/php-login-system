58326d884943fcfb1ac53ed9f93038b7dd90dd03
MohitKumar-stack <59208236+MohitKumar-stack@users.noreply.github.com>
2020-06-01 14:16:13 +0530

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -6,15 +6,19 @@ $pass =$_POST['pass'];
 
 // Create Your Database And Enter Details Like Root Name,Database Name And Password
 $server="localhost";
-$user="root";
+
+// WRITE YOUR DATABASE USER ID ,PASSWORD AND DATABASE NAME
+$user="";
 $password="";
-$db_name="xx"; 
+$db_name=""; 
+
+
 $conn =mysqli_connect($server,$user,$password,$db_name);
 
 
 if($conn){
     if ($email!=""and $pass!=""){
-        $query="SELECT email from click2 where email ='$email' and pass='$pass'";
+        $query="SELECT email from //HERE WRITE TABLE NAME where email ='$email' and pass='$pass'";
         $result=mysqli_query($conn,$query);
         $num =mysqli_num_rows($result);
             // echo $num;
